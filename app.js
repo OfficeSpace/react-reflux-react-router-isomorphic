17af2c870b09438c438047470604ada3a01f7db3
Amol Kelkar <amol@officespace.com>
2015-02-11 17:27:34 -0800

    separate server and client

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(9000, '127.0.0.1');
+require('node-jsx').install({harmony: true});
+require('./app/server');
