91dc9c09263ffcca1991feb691c129b2f0177471
Amol Kelkar <amol@officespace.com>
2015-02-11 18:28:19 -0800

    first react component

---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var App = module.exports = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello!</h1>
+    );
+  }
+});
+
--- app/server.js
+++ app/server.js
@@ -1,5 +1,16 @@
 var http = require('http');
+var React = require('react');
+var App = require('./components/App');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(
+    React.renderToString(
+      <html>
+        <body>
+          <App/>
+        </body>
+      </html>
+    )
+  );
 }).listen(9000, '127.0.0.1');
