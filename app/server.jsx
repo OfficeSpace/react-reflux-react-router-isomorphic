5e5dc5e2fd7176fc4b5098c476ba2d4324577688
Amol Kelkar <amol@officespace.com>
2015-02-13 15:34:39 -0800

    express with favicon, static files serving, body and cookie parsing

--- app/server.jsx
+++ app/server.jsx
@@ -1,10 +1,25 @@
 var http = require('http');
+var express = require('express')
+
 var React = require('react');
 var reactAsync = require('react-async')
-var App = require('./components/App');
-var debug = require('debug')('server');
+var App = require('./components/App')
+var debug = require('debug')('server')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var app = express()
 
-http.createServer(function (req, res) {
+app.use(logger('dev'))
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(favicon(path.join(__dirname, '../public/favicon.ico')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use(function(req, res, next) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   try {
     reactAsync.renderToStringAsync(
@@ -14,16 +29,29 @@ http.createServer(function (req, res) {
         </body>
       </html>, function(err, markup) {
         if(err) {
-          throw err
+          return next()
         }
         return res.end('<!DOCTYPE html>' + markup)
       }    
     )
   }
   catch(err) {
-    throw err
+    return next()
   }
 
-}).listen(9000, '127.0.0.1');
+})
 
-debug('Started')
+// handle errors
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+app.set('port', process.env.PORT || 9999);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
