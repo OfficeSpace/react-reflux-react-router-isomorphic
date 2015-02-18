82471f862716daa1e2b4270b9c27cf729899e07d
Amol Kelkar <amol@officespace.com>
2015-02-18 12:04:38 -0800

    Seed API

--- app/server.js
+++ app/server.js
@@ -16,12 +16,16 @@ var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var app = express()
 
+var api = require ('../api/api')
+
 app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, '../public')))
 app.use(favicon(path.join(__dirname, '../public/favicon.ico')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+api.routes(app)
 
 app.use(function(req, res, next) {
   res.writeHead(200, {'Content-Type': 'text/html'});
