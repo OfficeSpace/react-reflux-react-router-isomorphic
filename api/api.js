039d8745be2c894f466efc290bc1fa0890315aea
Amol Kelkar <amol@officespace.com>
2015-02-18 13:55:16 -0800

    Make dummy API call slow

--- api/api.js
+++ api/api.js
@@ -6,12 +6,13 @@ var api = {
   routes: function(app) {
     app.get('/api/building/:id', function(req, res, next) {
       debug('Request URL:', req.originalUrl)
-      res.type('application/json')
-      res.json({
-        "id":req.params.id,
-        "name": "Building " + req.params.id
-      })
-      next()
+      setTimeout(function() {
+        res.type('application/json')
+        res.json({
+          "id":req.params.id,
+          "name": "Building #" + req.params.id
+        })
+      }, 2000)
     })
   }
 }
